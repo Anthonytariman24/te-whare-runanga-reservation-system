178c5efc8e6743bef17a145b1cae1fc5f8b21503
JARP <105678913+anderson895@users.noreply.github.com>
2025-10-07 20:34:29 +0800

    Update settings.py

--- hotel_project/settings.py
+++ hotel_project/settings.py
@@ -8,7 +8,8 @@ DEBUG = True
 ALLOWED_HOSTS = [
     'localhost',
     '127.0.0.1',
-    'te-whare-runanga-reservation-system-six.vercel.app',
+    'te-whare-runanga-reservation-system.vercel.app',
+    'te-whare-runanga-reservation-system-bfyl3b3le.vercel.app',
 ]
 
 INSTALLED_APPS = [
